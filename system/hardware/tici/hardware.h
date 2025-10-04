da0b710f769da4b2128ff50b198c5d85e2cb7bec
herizon1054 <spivbaby@gmail.com>
2025-10-05 01:28:17 +0800

    Update hardware.h

--- system/hardware/tici/hardware.h
+++ system/hardware/tici/hardware.h
@@ -59,7 +59,7 @@ public:
   static void reboot() { std::system("sudo reboot"); }
   static void poweroff() { std::system("sudo poweroff"); }
   static void set_brightness(int percent) {
-    percent = std::clamp(percent, 1, 65);  // 限制亮度在 1%~65% 之間
+    percent = std::clamp(percent, 1, 60);  // 限制亮度在 1%~60% 之間
     float max = std::stof(util::read_file("/sys/class/backlight/panel0-backlight/max_brightness"));
     std::ofstream("/sys/class/backlight/panel0-backlight/brightness") << int(percent * (max / 100.0f)) << "\n";
   }
