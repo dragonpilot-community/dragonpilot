55fa4c6fd5534ea7a67b8d2294d6f192b8eb0326
FIT 4 e-HEV <yihang87184@gmail.com>
2026-01-06 02:13:35 +0800

    Adjust lateral tuning parameters for Honda Fit EHEV

--- opendbc_repo/opendbc/car/honda/interface.py
+++ opendbc_repo/opendbc/car/honda/interface.py
@@ -174,8 +174,8 @@ class CarInterface(CarInterfaceBase):
     # ========================
     elif candidate == CAR.HONDA_FIT_EHEV_2021:
       ret.lateralParams.torqueBP, ret.lateralParams.torqueV = [[0, 4096], [0, 4096]]
-      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.15], [0.04]]
-      #ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.25], [0.08]]
+      #ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.15], [0.04]]
+      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.25], [0.08]]
       ret.lateralTuning.pid.kf = 0.0
       ret.steerActuatorDelay = 0.0
       ret.maxLateralAccel = 2.5
