3d1671a66e4a8e8378ff59eeccf9937777128e82
FIT 4 e-HEV <yihang87184@gmail.com>
2026-01-06 02:18:16 +0800

    Update values.py

--- opendbc_repo/opendbc/car/honda/values.py
+++ opendbc_repo/opendbc/car/honda/values.py
@@ -233,7 +233,7 @@ class CAR(Platforms):
   #==========================================================================
   HONDA_FIT_EHEV_2021 = HondaBoschPlatformConfig(
     [HondaCarDocs("Honda Fit e:HEV 2021", "All")],
-    CarSpecs(mass=2645 * CV.LB_TO_KG, wheelbase=2.53, steerRatio=17.5, centerToFrontRatio=0.44, tireStiffnessFactor=0.5, minSteerSpeed=0. * CV.KPH_TO_MS),
+    CarSpecs(mass=2645 * CV.LB_TO_KG, wheelbase=2.53, steerRatio=15.2, centerToFrontRatio=0.44, tireStiffnessFactor=0.5, minSteerSpeed=0. * CV.KPH_TO_MS),
     {Bus.pt: 'honda_bosch_radarless_generated'},
     flags=HondaFlags.BOSCH_RADARLESS,
   )
