1bd1c1c6198d8c5cc0d904dc48260a8a7de6cf31
FIT 4 e-HEV <yihang87184@gmail.com>
2026-01-13 01:44:31 +0800

    Update hondacan.py

--- opendbc_repo/opendbc/car/honda/hondacan.py
+++ opendbc_repo/opendbc/car/honda/hondacan.py
@@ -168,26 +168,33 @@ def create_acc_hud(packer, bus, CP, enabled, pcm_speed, pcm_accel, hud_control, 
 def create_lkas_hud(packer, bus, CP, hud_control, lat_active, steering_available, reduced_steering, alert_steer_required, lkas_hud):
   commands = []
 
-  # --- 修正版代碼 ---
-  # 只有當 OP 真的在控制轉向 (lat_active) 時，才顯示實線
-  # 這樣 EPS 收到實線訊號時，同時也會收到扭矩請求，就不會報錯
-  is_solid_lanes = 1 if lat_active else hud_control.lanesVisible
+  # --- 修正版 (解決橘燈報錯) ---
+  # 邏輯：絕對互斥。
+  # 如果 OpenPilot 在轉向 (lat_active)：顯示實線，隱藏虛線，關閉警告。
+  # 如果 OpenPilot 沒動作 (Standby)：顯示虛線，隱藏實線。
   
+  if lat_active:
+    is_solid = 1
+    is_dashed = 0
+    is_steer_req = 0
+  else:
+    is_solid = 0
+    is_dashed = hud_control.lanesVisible
+    is_steer_req = alert_steer_required
+
   lkas_hud_values = {
     'LKAS_READY': 1,
     'LKAS_STATE_CHANGE': 1,
-    'STEERING_REQUIRED': 0 if lat_active else alert_steer_required, # 有轉向時關閉警告
-    'SOLID_LANES': is_solid_lanes,
+    'STEERING_REQUIRED': is_steer_req,
+    'SOLID_LANES': is_solid,
     'BEEP': 0,
   }
 
   if CP.carFingerprint in (HONDA_BOSCH_RADARLESS | HONDA_BOSCH_CANFD):
     lkas_hud_values['LANE_LINES'] = 3
-    # 這是關鍵：針對 Bosch Radarless
-    # 如果正在轉向(Active)，強制把虛線隱藏(0)，只顯示實線
-    # 如果沒在轉向(Standby)，才顯示原本的虛線
-    lkas_hud_values['DASHED_LANES'] = 0 if lat_active else hud_control.lanesVisible
+    lkas_hud_values['DASHED_LANES'] = is_dashed # 確保這裡引用上面的邏輯
     
+    # 務必保留原廠的錯誤訊號轉發，不然也會亮橘燈
     if CP.carFingerprint in HONDA_BOSCH_RADARLESS:
       lkas_hud_values['LKAS_PROBLEM'] = lkas_hud['LKAS_PROBLEM']
   # --- 修正結束 ---
