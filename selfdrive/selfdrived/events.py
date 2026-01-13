1b8320ec086dd83e73269b2b4c248f3065858eb0
FIT 4 e-HEV <yihang87184@gmail.com>
2026-01-13 21:45:51 +0800

    Update events.py

--- selfdrive/selfdrived/events.py
+++ selfdrive/selfdrived/events.py
@@ -245,7 +245,7 @@ def startup_master_alert(CP: car.CarParams, CS: car.CarState, sm: messaging.SubM
 def below_engage_speed_alert(CP: car.CarParams, CS: car.CarState, sm: messaging.SubMaster, metric: bool, soft_disable_time: int, personality) -> Alert:
   return NoEntryAlert(f"Drive above {get_display_speed(CP.minEnableSpeed, metric)} to engage")
 
-
+#======================================================================================================
 def below_steer_speed_alert(CP: car.CarParams, CS: car.CarState, sm: messaging.SubMaster, metric: bool, soft_disable_time: int, personality) -> Alert:
   return Alert(
     f"Steer Assist Unavailable Below {get_display_speed(CP.minSteerSpeed, metric)}",
@@ -253,7 +253,7 @@ def below_steer_speed_alert(CP: car.CarParams, CS: car.CarState, sm: messaging.S
     AlertStatus.normal, AlertSize.small,      # <--- 改為 .normal (變黑底)
     Priority.LOW, VisualAlert.none, AudibleAlert.none, 0.4)   # <--- 改為 .none (靜音)
 
-
+#======================================================================================================
 def calibration_incomplete_alert(CP: car.CarParams, CS: car.CarState, sm: messaging.SubMaster, metric: bool, soft_disable_time: int, personality) -> Alert:
   first_word = 'Recalibrating' if sm['liveCalibration'].calStatus == log.LiveCalibrationData.Status.recalibrating else 'Calibrating'
   return Alert(
